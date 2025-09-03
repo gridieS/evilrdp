bd1e6b4ca022a0db21337109445a69760c19b450
gridieS <gridieworks@gmail.com>
2025-09-03 05:39:49 +0300

    removed pillow requirement

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
 import re
 import platform
 
-VERSIONFILE="evilrdp/_version.py"
+VERSIONFILE = "evilrdp/_version.py"
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
@@ -13,51 +13,39 @@ else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 setup(
-	# Application name:
-	name="evilrdp",
-
-	# Version number (initial):
-	version=verstr,
-
-	# Application author details:
-	author="Tamas Jos",
-	author_email="info@skelsecprojects.com",
-
-	# Packages
-	packages=find_packages(),
-
-	# Include additional files into the package
-	include_package_data=True,
-
-
-	# Details
-	url="https://github.com/skelsec/evilrdp",
-
-	zip_safe = False,
-	#
-	# license="LICENSE.txt",
-	description="evilrdp",
-
-	# long_description=open("README.txt").read(),
-	python_requires='>=3.7',
-
-	install_requires=[
-        'pillow==9.5.0'
-		'prompt-toolkit',
-		'aardwolf>=0.2.5',
-		'pyqt5',
-		'pyqt5-sip',
-		'pyperclip',
-	],
-	
-	classifiers=[
-		"Programming Language :: Python :: 3.8",
-		"Operating System :: OS Independent",
-	],
-	entry_points={
-		'console_scripts': [
-			'evilrdp = evilrdp.__main__:main',
-		],
-
-	}
+    # Application name:
+    name="evilrdp",
+    # Version number (initial):
+    version=verstr,
+    # Application author details:
+    author="Tamas Jos",
+    author_email="info@skelsecprojects.com",
+    # Packages
+    packages=find_packages(),
+    # Include additional files into the package
+    include_package_data=True,
+    # Details
+    url="https://github.com/skelsec/evilrdp",
+    zip_safe=False,
+    #
+    # license="LICENSE.txt",
+    description="evilrdp",
+    # long_description=open("README.txt").read(),
+    python_requires=">=3.7",
+    install_requires=[
+        "prompt-toolkit",
+        "aardwolf>=0.2.5",
+        "pyqt5",
+        "pyqt5-sip",
+        "pyperclip",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "evilrdp = evilrdp.__main__:main",
+        ],
+    },
 )
